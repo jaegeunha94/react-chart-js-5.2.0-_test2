d80383cea43f527922b49e48814979e2cf5699cc
jaegeunha94 <46130542+jaegeunha94@users.noreply.github.com>
2023-09-12 15:34:45 +0900

    Remove Chartline

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './style.css';
 
 import CharBar from './CharBar';
-import CharLine from './CharLine';
 
 export default function App() {
   return (
     <div>
       <CharBar />
-      <CharLine />
     </div>
   );
 }
