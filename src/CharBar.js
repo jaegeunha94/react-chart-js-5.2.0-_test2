50f892f59594974caa1153153c77b85cce8ef2c4
jaegeunha94 <46130542+jaegeunha94@users.noreply.github.com>
2023-09-13 11:12:03 +0900

    Add Data

--- src/CharBar.js
+++ src/CharBar.js
@@ -43,11 +43,18 @@ const data = {
   datasets: [
     {
       data: [2000, 1250, 1750, 1000, 2250, 2400, 2500],
-      label: '',
+      label: 'Dataset 1',
+      borderColor: 'rgb(255, 99, 132)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
       hoverBackgroundColor: 'blue',
       borderRadius: 5,
       barThickness: 48,
+    },
+    {
+      label: 'Dataset 2',
+      data: [2000, 1250, 1750, 1000, 2250, 2400, 2500],
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
     },
   ],
 };
