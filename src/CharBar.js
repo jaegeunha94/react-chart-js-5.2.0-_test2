22c2cdf79ed3947ac37f72649af5bad6a95cee19
jaegeunha94 <46130542+jaegeunha94@users.noreply.github.com>
2023-09-14 11:28:39 +0900

    1 file modified

--- src/CharBar.js
+++ src/CharBar.js
@@ -36,20 +36,24 @@ const data = {
   labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
   datasets: [
     {
-      data: [2000, 1250, 1750, , 1000, 2250, 2400, 2500],
+      data: [2000, 1250, 1750, , 10, 1000, 2250, 2400],
+      // data: [2000, 1250, 1750, , -10, 1000, 2250, 2400],
       label: 'Dataset 1',
       borderColor: 'rgb(255, 99, 132)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      borderSkipped: 'true',
+      // borderSkipped: 'true',
+      // borderSkipped: 'middle',
+      // borderSkipped: 'bottom',
       // hoverBackgroundColor: 'blue',
       // borderWidth: 5
       // borderRadius: 5,
       // barThickness: 48,
       // minBarLength: 200,
+      pointStyle: 'triangle',
     },
     {
       label: 'Dataset 2',
-      data: [2000, 1250, 1750, 1000, 2250, 2400, 2500],
+      data: [2000, 1250, 1750, , 1000, 2250, 2400],
       borderColor: 'rgb(53, 162, 235)',
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
       pointStyle: 'rect',
