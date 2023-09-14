3e85a430904ea7624204ee6fbc8e9b5401a533f9
jaegeunha94 <46130542+jaegeunha94@users.noreply.github.com>
2023-09-14 11:16:02 +0900

    Add Styling

--- src/CharBar.js
+++ src/CharBar.js
@@ -36,19 +36,23 @@ const data = {
   labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
   datasets: [
     {
-      data: [2000, 1250, 1750, 1000, 2250, 2400, 2500],
+      data: [2000, 1250, 1750, , 1000, 2250, 2400, 2500],
       label: 'Dataset 1',
       borderColor: 'rgb(255, 99, 132)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      borderSkipped: 'true',
       // hoverBackgroundColor: 'blue',
+      // borderWidth: 5
       // borderRadius: 5,
       // barThickness: 48,
+      // minBarLength: 200,
     },
     {
       label: 'Dataset 2',
       data: [2000, 1250, 1750, 1000, 2250, 2400, 2500],
       borderColor: 'rgb(53, 162, 235)',
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      pointStyle: 'rect',
     },
   ],
 };
